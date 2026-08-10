04b95f41f87b1413ba5422a7df9892615fc91747
Vadim Gerasimov <angryvadik@gmail.com>
2026-08-11 00:00:28 +0200

    fix: hide scrollbar gutter in watch layout

--- src/layouts/WatchLayout.tsx
+++ src/layouts/WatchLayout.tsx
@@ -1,5 +1,9 @@
 import type { ParentProps } from "solid-js";
 
 export default function WatchLayout(props: ParentProps) {
-  return <main class="h-dvh overflow-hidden bg-black">{props.children}</main>;
+  return (
+    <main data-no-scrollbar-gutter class="h-dvh overflow-hidden bg-black">
+      {props.children}
+    </main>
+  );
 }
